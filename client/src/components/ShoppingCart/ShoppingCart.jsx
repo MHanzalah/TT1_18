669cc39bcd280b1764e3a26dbc85543b855c1fe2
MdHanzalah <hanzalah.mazlan@gmail.com>
2021-09-30 13:06:55 +0800

    Added an empty ShoppingCart Component

---
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+
+
+import useStyles from './styles';
+
+const ShoppingCart = ({  }) => {
+  const classes = useStyles();
+
+
+  return (
+    <div class="shoppingcart"></div>
+  );
+};
+
+export default ShoppingCart;
