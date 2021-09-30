587e0d7f19799ebf84c9ac291a73cb956c753fa7
zhenghui <zhenghui6696@gmail.com>
2021-09-30 14:46:13 +0800

    Adding Item Attributes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Navbar from './components/Navbar/Navbar'
 import Products from './components/Products/Products'
+import ItemID from './components/DisplayItem/ItemID';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Products />
-      <Navbar />
-    </div>
+    <Router>
+    <>
+      <Switch>
+        <Route path="/item-:id" component={ItemID} />
+      </Switch>
+    </>
+  </Router>
+    // <div className="App">
+    //   <Products />
+    //   <Navbar />
+    // </div>
   );
 }
 
