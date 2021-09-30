60598237efeb4aef3a73f477eaa17957e6f6de67
limchuenyin <limchuenyin@gmail.com>
2021-09-30 11:55:01 +0800

    initialize react frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar/Navbar'
+import Products from './components/Products/Products'
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
