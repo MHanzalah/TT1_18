4ece82ae02174482ad25746078b34d62ac18ef6b
arupashree21 <arupashree2000@gmail.com>
2021-09-30 14:44:27 +0800

    Login_component_update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './style.css';
+import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar'
-import Products from './components/Products/Products'
-
+import Product from './components/Products/Products'
 function App() {
   return (
-    <div className="App">
-      <Products />
-      <Navbar />
-    </div>
+    <>
+      <Navbar/>
+      <Product/>
+      {/* Login-component
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Login />
+        </div>
+      </div>*/}
+      
+    </>
   );
 }
 
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+      <>
+     
+      
+        <div className="Login-form">
+        <p>Login</p>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" controlId="email">
+            <Form.Label>Email </Form.Label>
+            <br />
+            <Form.Control
+                autoFocus
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+            <Form.Label>Password </Form.Label>
+            <br />
+            <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            </Form.Group>
+            <br />
+            <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Login
+            </Button>
+
+            <p>
+            Forgot <a href="#">password?</a>
+            </p>
+        </Form>
+        </div>
+    </>
+  );
+}
+
+export default Login;
