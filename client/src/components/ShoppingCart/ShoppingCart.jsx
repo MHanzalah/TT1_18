e5ee12a79a71391df6e212d411a8784c3468af0f
MdHanzalah <hanzalah.mazlan@gmail.com>
2021-09-30 16:03:03 +0800

    Updated Shopping Cart component. Added raw data (hardcoded) and included other required buttons and additional information

--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -6,39 +6,55 @@ import AddIcon from '@material-ui/icons/Add'
 import RemoveIcon from '@material-ui/icons/Remove'
 import DeleteIcon from '@material-ui/icons/Delete'
 
+import Product from './Products'
+
+
 import useStyles from './styles';
 
-const ShoppingCart = ({ }) => {
+const ShoppingCart = ({ products }) => {
   const classes = useStyles();
 
   // add number of item - to be done
   const addToCart = () => {
-
+    // Increase quantity of number in card 
+    // Recalculate Price
   }
 
   // reduce number of item - to be done
   const removeFromCart = () => {
-
+    // Reduce quantity of number in card 
+    // Recalculate Price
   }
 
   // remove item from cart - to be done
   const deleteFromCart = () => {
-
+    // Totally remove item from card
   }  
 
   return (
+    <Card className={classes.root}>
     <Card className={classes.productCard} variant="outlined">
       <CardContent> 
-        <Typography variant="h5" component="h2">
-          Men's Bag
+
+        <Typography variant="h7" component="h2">
+          Mens Casual Premium Slim Fit T-Shirts 
         </Typography>
+
+        <CardMedia className={classes.Media} component="img" image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""/>
+
         <Typography color="textSecondary">
-          $500
+          Price: $22.30
         </Typography>
+
         <Typography color="textSecondary">Quantity: 1</Typography>
+
+        <Typography color="textSecondary">
+          Total: $22.30
+        </Typography>
+
       </CardContent>
       <CardActions>
-        <ButtonGroup
+        <ButtonGroup className={classes.BtnGrp}
           variant="contained"
           color="primary"
           aria-label="contained primary button group"
@@ -69,7 +85,15 @@ const ShoppingCart = ({ }) => {
           </Button>
         </ButtonGroup>
       </CardActions>
+
     </Card>
+
+    <Card className={classes.totalprice}>
+      <Typography color="textSecondary"> Total: $22.30 </Typography>      
+      <Button variant="contained" color="primary" disabled>Checkout</Button>
+    </Card>
+
+  </Card>
   )
 };
 
